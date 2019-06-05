275fa95e62c348ac9c6f7880da1dda1b204ca294
Herman Kamper <kamperh@gmail.com>
2019-06-05 11:07:44 +0200

    Docker shell scripts

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run --runtime=nvidia -it --rm -u $(id -u):$(id -g) -p 8887:8887 \
-    -v /r2d2/backup/endgame/datasets/buckeye:/data/buckeye \
-    -v /r2d2/backup/endgame/datasets/zrsc2015/xitsonga_wavs:/data/xitsonga_wavs \
+    -v /home/kamperh/backup/endgame/datasets/buckeye:/data/buckeye \
+    -v /home/kamperh/backup/endgame/datasets/zrsc2015/xitsonga_wavs:/data/xitsonga_wavs \
     -v "$(pwd)":/home \
     py3.6_tf1.13
--- docker_notebook.sh
+++ docker_notebook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker run --runtime=nvidia --rm -it -p 8889:8889 \
-    -v /r2d2/backup/endgame/datasets/buckeye:/data/buckeye \
-    -v /r2d2/backup/endgame/datasets/zrsc2015/xitsonga_wavs:/data/xitsonga_wavs \
+docker run --runtime=nvidia -it --rm -u $(id -u):$(id -g) -p 8889:8889 \
+    -v /home/kamperh/backup/endgame/datasets/buckeye:/data/buckeye \
+    -v /home/kamperh/backup/endgame/datasets/zrsc2015/xitsonga_wavs:/data/xitsonga_wavs \
     -v "$(pwd)":/home \
     py3.6_tf1.13 \
     bash -c "ipython notebook --no-browser --ip=0.0.0.0 --allow-root --port=8889"
